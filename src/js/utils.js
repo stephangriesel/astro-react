05fa4aa79296b202bed03795c197dd6fdd82e977
Stephan Griesel <sgriesel@gmail.com>
2023-04-07 06:06:19 +0200

    filter posts continued

--- src/js/utils.js
+++ src/js/utils.js
@@ -23,5 +23,23 @@ export function formatBlogPosts(posts, {
   sortByDate = true,
   limit = undefined
 } = {}){
-  console.log(posts)
+  const filteredPosts = posts.reduce((acc,post) => {
+    const { date, draft } = post.frontmatter;
+    if (filterOutDrafts && draft) return acc;
+    if (filterOutFuturePosts && new Date(date) > new Date ()) 
+    return acc;
+    acc.push(post)
+    return acc;
+  },[])
+
+  if(sortByDate){
+    filteredPosts.sort((a,b) => new Date(b.frontmatter.date - new Date(a.frontmatter.date)))
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5)
+  }
+
+  if(typeof limit === "number"){
+    return filteredPosts.slice(0,limit)
+  }
+  return filteredPosts
 }
