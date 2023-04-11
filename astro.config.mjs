7cd0b886b01037103770faa18b98cafb90925149
Stephan Griesel <sgriesel@gmail.com>
2023-04-11 06:59:44 +0200

    config sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://master--verdant-medovik-e9bb32.netlify.app/',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }
-  )]
+  }), sitemap({
+    filter: (page) => page !== 'https://master--verdant-medovik-e9bb32.netlify.app/about/'
+  })]
 });
